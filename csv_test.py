c1dac7ad271744ee5a07f079ad035b590ad2b781
mmh <mhry@igr.poznan.pl>
2025-06-04 23:18:02 +0200

    modified basic_write() logic

--- csv_test.py
+++
@@ -1,7 +0,0 @@
-import io
-
-data = [[str(k) for k in range(10)] for i in range(2048)]
-
-with io.open('test.txt', 'w', buffering=524_288) as f:
-    for line in data:
-        f.write(f"{'\t'.join(line)}\n")
--- slipper.py
+++ slipper.py
@@ -3,6 +3,10 @@
 import argparse, sys, io, gzip
 import pysam
 
+#
+# CLI argument parsing
+#
+
 parser = argparse.ArgumentParser(
     prog="Slipper",
     description="Extract soft clipped nucleotides from BAM",
@@ -35,50 +39,92 @@ parser.add_argument(
 Gzip compression level (default=1)'''
 )
 
-# write functions
+#
+# File writing functions
+#
 
-def default_write(write_file):
-    write_file.write(f"readID\tflags\tclipEnd\tisReverse\tsoftClipLen\tclipSeq\n")
+def basic_write(write_file: str) -> None:
+    """
+    Basic function for writing the output tsv file, it's used for: buffered
+    plain text tsv writing, gzip compressed tsv writing.
 
-    id = 0
+    Args:
+        write_file (str): name of the output file
+
+    Returns:
+        None
+    """
+    # write column headers to output file
+    write_file.write(f"QNAME\tFLAGS\tCLIP5\tCLIP3\tPOS\tSEQ\n")
 
     for read in bamfile.fetch():
+
+        # initialize dictionary with row values, all values must be strings
+        r_dict = {
+            "QNAME": read.query_name,
+            "FLAGS": f"{read.flag}:{read.flag:016b}",
+            "CLIP5": "NA",
+            "CLIP5": "NA",
+            "POS": str(read.reference_start),
+            "SEQ": "NA"
+        }
+
+        # get CIGAR string in tuple format
+        # (4, 7) is the same as 7S
         cigar = read.cigartuples
-        flags = f"{read.flag}:{read.flag:016b}" # read flags as 0 padded 16bit binary
-        is_rev = read.is_reverse
 
-        if cigar[-1][0] == 4:
-            strand_end = "clip3"
-            sfc_idx = cigar[-1][1] # number of terminal soft clipped bases
-            seq = read.query_sequence[-sfc_idx:]
-
-            write_file.write(f"{id}\t{flags}\t{strand_end}\t{is_rev}\t{sfc_idx}\t{seq}\n")
-
+        # get 5' clip sequence
         if cigar[0][0] == 4:
-            strand_end = "clip5"
-            sfc_idx = cigar[0][1] # number of terminal soft clipped bases
-            seq = read.query_sequence[sfc_idx:]
+            r_dict["CLIP5"] = read.query_sequence[cigar[0][1]:]
 
-            write_file.write(f"{id}\t{flags}\t{strand_end}\t{is_rev}\t{sfc_idx}\t{seq}\n")
+        # get 3' clip sequence
+        if cigar[-1][0] == 4:
+            r_dict["CLIP3"] = read.query_sequence[-cigar[-1][1]:]
 
         else:
             pass
+        
+        # write row into the output tsv file
+        write_file.write('\t'.join(r_dict[k] for k in r_dict) + '\n')
 
-        id += 1
+def plain_write(out_name: str) -> None:
+    """
+    Writes the output into a plain text file
 
-def gzip_write(out_name: str, c_level: int):
-    with gzip.open(out_name, "wt", compresslevel=c_level) as f:
-        default_write(f)
+    Args:
+        out_name (str): name of the output file
 
-def plain_write(out_name: str):
+    Returns:
+        None
+    """
     with io.open(out_name, 'w', buffering=524_288) as f:
-        default_write(f)
+        basic_write(f)
+
+def gzip_write(out_name: str, c_level: int) -> None:
+    """
+    Writes the output into a gzip compressed plain text file
+
+    Args:
+        out_name (str): name of the output file
+        c_level (int): gzip compression level, default argparse value sets it to 1
+
+    Returns:
+        None
+    """
+    with gzip.open(out_name, "wt", compresslevel=c_level) as f:
+        basic_write(f)
+
+#
+# Main logic
+#
 
 if __name__ == "__main__":
     args = parser.parse_args()
 
+    # open input BAM for reading
     bamfile = pysam.AlignmentFile(args.input, "rb")
 
+    # write processed output to .tsv or .tsv.gz based on the `-g` switch
     if args.gzip:
         out_name = f"{args.output}.tsv.gz"
         gzip_write(out_name, args.compress)
@@ -86,5 +132,6 @@ if __name__ == "__main__":
     else:
         out_name = f"{args.output}.tsv"
         plain_write(out_name)
-            
+    
+    # close input BAM
     bamfile.close()
