c0e7d9e932e68b5864f198a5a4ccb9388db5b7f2
mmh <mhry@igr.poznan.pl>
2025-06-05 01:38:56 +0200

    fixed CLIP5 extraction indexing error, documentation changes

--- slipper.py
+++ slipper.py
@@ -17,7 +17,7 @@ parser.add_argument(
     required=True,
     type=str,
     help='''
-Path to the input BAM file'''
+Path to the input BAM file (needs an index)'''
 )
 parser.add_argument(
     "-o", "--output",
@@ -38,34 +38,44 @@ parser.add_argument(
     help='''
 Gzip compression level (default=1)'''
 )
+parser.add_argument(
+    "-s", "--with-sequence",
+    required=False, action="store_true",
+    help='''
+Store the whole read sequence (SEQ) in the last column. Default is off (fill
+with "NA")'''
+)
 
 #
 # File writing functions
 #
 
-def basic_write(write_file: str) -> None:
+def basic_write(write_file: str, seq_switch: bool) -> None:
     """
     Basic function for writing the output tsv file, it's used for: buffered
     plain text tsv writing, gzip compressed tsv writing.
 
     Args:
         write_file (str): name of the output file
+        seq_switch (bool): controls whether SEQ column should contain whole read
+            sequence or NA
 
     Returns:
         None
     """
     # write column headers to output file
-    write_file.write(f"QNAME\tFLAGS\tCLIP5\tCLIP3\tPOS\tSEQ\n")
+    write_file.write(f"QNAME\tFLAGS\tRNAME\tPOS\tCLIP5\tCLIP3\tSEQ\n")
 
     for read in bamfile.fetch():
 
         # initialize dictionary with row values, all values must be strings
         r_dict = {
             "QNAME": read.query_name,
-            "FLAGS": f"{read.flag}:{read.flag:016b}",
-            "CLIP5": "NA",
-            "CLIP5": "NA",
+            "FLAGS": f"{read.flag}:{read.flag:012b}",
+            "RNAME": read.reference_name,
             "POS": str(read.reference_start),
+            "CLIP5": "NA",
+            "CLIP3": "NA",
             "SEQ": "NA"
         }
 
@@ -73,9 +83,13 @@ def basic_write(write_file: str) -> None:
         # (4, 7) is the same as 7S
         cigar = read.cigartuples
 
+        # get whole read sequence if arg switch is on
+        if seq_switch:
+            r_dict["SEQ"] = read.query_sequence
+
         # get 5' clip sequence
         if cigar[0][0] == 4:
-            r_dict["CLIP5"] = read.query_sequence[cigar[0][1]:]
+            r_dict["CLIP5"] = read.query_sequence[:cigar[0][1]]
 
         # get 3' clip sequence
         if cigar[-1][0] == 4:
@@ -87,7 +101,7 @@ def basic_write(write_file: str) -> None:
         # write row into the output tsv file
         write_file.write('\t'.join(r_dict[k] for k in r_dict) + '\n')
 
-def plain_write(out_name: str) -> None:
+def plain_write(out_name: str, seq_switch: bool) -> None:
     """
     Writes the output into a plain text file
 
@@ -98,9 +112,9 @@ def plain_write(out_name: str) -> None:
         None
     """
     with io.open(out_name, 'w', buffering=524_288) as f:
-        basic_write(f)
+        basic_write(f, seq_switch)
 
-def gzip_write(out_name: str, c_level: int) -> None:
+def gzip_write(out_name: str, seq_switch: bool, gzip_level: int) -> None:
     """
     Writes the output into a gzip compressed plain text file
 
@@ -111,8 +125,8 @@ def gzip_write(out_name: str, c_level: int) -> None:
     Returns:
         None
     """
-    with gzip.open(out_name, "wt", compresslevel=c_level) as f:
-        basic_write(f)
+    with gzip.open(out_name, "wt", compresslevel=gzip_level) as f:
+        basic_write(f, seq_switch)
 
 #
 # Main logic
@@ -127,11 +141,18 @@ if __name__ == "__main__":
     # write processed output to .tsv or .tsv.gz based on the `-g` switch
     if args.gzip:
         out_name = f"{args.output}.tsv.gz"
-        gzip_write(out_name, args.compress)
+        gzip_write(
+            out_name=out_name,
+            seq_switch=args.with_sequence,
+            gzip_level=args.compress
+        )
 
     else:
         out_name = f"{args.output}.tsv"
-        plain_write(out_name)
+        plain_write(
+            out_name=out_name,
+            seq_switch=args.with_sequence
+        )
     
     # close input BAM
     bamfile.close()
