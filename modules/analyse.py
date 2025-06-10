488195159dcb697d672a53ce81afc5ab01ed8b86
mmh <mhry@igr.poznan.pl>
2025-06-10 03:10:32 +0200

    added functions to analyze.py, output is not yet complete

--- modules/analyse.py
+++ modules/analyse.py
@@ -7,16 +7,17 @@ analyse.py, handles the analysis of 'extract' output
 
 '''
 What this needs to do:
-    * calculate tail length
-    * assign strand
-    * assign tail type:
-        all G or C -> G/C
-        Empty (len = 0) -> no_tail
-        all T -> polyU
-        all A -> polyA
-        A or T -> mixed_AU
-        otherwise -> other
-    * Count terminal Us (Ts)
+    * calculate tail length             DONE
+    * count nucleotides                 DONE
+    * assign strand                     DONE
+    * assign tail type:                 DONE
+        all G or C -> G/C               DONE
+        Empty (len = 0) -> no_tail      DONE
+        all T -> polyU                  DONE
+        all A -> polyA                  DONE
+        A or T -> mixed_AU              DONE
+        otherwise -> other              DONE
+    * Count terminal Us (Ts)            DONE
 '''
 
 @dataclass
@@ -49,14 +50,60 @@ def count_bases(line: list[str, ...]) -> tuple[int, int, int, int]:
     else:
         return tuple([0, 0, 0, 0])
 
+def get_strand(line: list[str, ...]) -> int:
+    flags = bin(int(line[1].split(':')[0]))
+    return -1 if flags[-5] else 1
+
+def get_tail_type(base_counts: tuple[int, int, int, int]) -> str:
+    '''
+    Args:
+        base_counts (tuple): (A, C, G, T)
+    '''
+    total = sum(base_counts)
+    if total == 0:
+        return "no_tail"
+
+    elif base_counts[3] == total:
+        return "polyU"
+
+    elif base_counts[0] == total:
+        return "polyA"
+
+    elif base_counts[0] + base_counts[3] == total:
+        return "mixed_AU"
+
+    elif base_counts[2] + base_counts[3] == total:
+        return "mixed_GC"
+
+    else:
+        return "other"
+
+def count_Us(line: list[str]) -> int:
+    tail = line[5]
+    u_count = 0
+    for i in range(1, len(tail) + 1):
+        if tail[-i] == 'T':
+            u_count += 1
+        else:
+            break
+
+    return u_count
+
 def main_testing(infile: str) -> None:
 
-    with gzip.open(infile, "rt") as f:
-        for line in f:
+    with gzip.open(infile, "rt") as fi, gzip.open("analyse_out.tsv.gz", "wt") as fo:
+        header = fi.readline() # throw away the header
+        for line in fi:
             if line != '':
-                print(line)
-                print(get_tail_len(line))
-                print(count_bases(line))
+                l = line.strip().split('\t')
+                print(l)
+                print(get_tail_len(l))
+                counts = count_bases(l)
+                print(get_strand(l))
+                print(get_tail_type(counts))
+                print(count_Us(l))
+
+                fo.write(f"{line.strip()}\t{get_tail_type(counts)}\n")
 
 #        for line in f:
 #            print(line.strip().split('\t'))
